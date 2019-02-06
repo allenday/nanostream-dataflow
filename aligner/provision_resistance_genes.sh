799274140fbb00e9554675ebba8fe0837593349c
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-02-06 14:10:11 +1000

    localisation

--- aligner/provision_resistance_genes.sh
+++ aligner/provision_resistance_genes.sh
@@ -10,8 +10,8 @@ export MIN_REPLICAS=1
 export MAX_REPLICAS=3
 export TARGET_CPU_UTILIZATION=0.5
 
-export DOCKER_IMAGE='allenday/bwa-http-docker:http'
-export BWA_FILES='gs://nano-stream1/ResistanceGenes/resFinder/*'
+export DOCKER_IMAGE='dockersubtest/nano-larry:http'
+export BWA_FILES='gs://nano-stream1/NewDatabases/resFinder/*'
 
 source provision.sh
 
