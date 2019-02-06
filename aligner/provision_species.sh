01c811de789e4e2c0df737f0b1e890f0ecf92db3
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-02-06 16:50:21 +1000

    Update provision_species.sh

--- aligner/provision_species.sh
+++ aligner/provision_species.sh
@@ -3,8 +3,8 @@ set -e
 
 # set environment variables
 export NAME="bwa-species"
-export REGION="asia-northeast1-c"
-export ZONE="${REGION}-b"
+export REGION="asia-northeast1"
+export ZONE="${REGION}-c"
 export MACHINE_TYPE="n1-standard-8"
 export MIN_REPLICAS=1
 export MAX_REPLICAS=3
