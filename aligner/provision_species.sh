61efa09e984220f2c2dbcf796697871a37e2e48a
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-02-06 13:53:41 +1000

    localised variables

--- aligner/provision_species.sh
+++ aligner/provision_species.sh
@@ -3,7 +3,7 @@ set -e
 
 # set environment variables
 export NAME="bwa-species"
-export REGION="europe-west3"
+export REGION="asia-northeast1-c"
 export ZONE="${REGION}-b"
 export MACHINE_TYPE="n1-standard-8"
 export MIN_REPLICAS=1
@@ -11,7 +11,7 @@ export MAX_REPLICAS=3
 export TARGET_CPU_UTILIZATION=0.5
 
 export DOCKER_IMAGE='allenday/bwa-http-docker:http'
-export BWA_FILES='gs://nano-stream-test/CombinedDatabases/*'
+export BWA_FILES='gs://nano-stream1/NewDatabases/*'
 
 source provision.sh
 
