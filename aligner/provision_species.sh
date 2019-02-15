6eb5e03cf8b58caa22af54906e4639a2c35d93c4
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-02-15 16:08:43 +1000

    switch to CombinedDatabases

--- aligner/provision_species.sh
+++ aligner/provision_species.sh
@@ -11,7 +11,7 @@ export MAX_REPLICAS=3
 export TARGET_CPU_UTILIZATION=0.7
 
 export DOCKER_IMAGE='allenday/bwa-http-docker:http'
-export BWA_FILES='gs://nano-stream1/NewDatabases/*'
+export BWA_FILES='gs://nano-stream1/CombinedDatabases/*'
 
 # REQUESTER_PROJECT - project billed for downloading BWA_FILES
 # this line set it value to the active project ID
