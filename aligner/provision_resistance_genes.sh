56c594f837a0ff70b42a1859154d4c333cd4297d
Firedrops <31351436+Firedrops@users.noreply.github.com>
2019-04-24 16:40:57 +1000

    Re-Localise after pubsub monitor pull request

---
+++ aligner/provision_resistance_genes.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# set environment variables
+export NAME="bwa-resistance-genes"
+export REGION="us-central1"
+export ZONE="${REGION}-b"
+export MACHINE_TYPE="n1-standard-1"
+export MIN_REPLICAS=1
+export MAX_REPLICAS=3
+export TARGET_CPU_UTILIZATION=0.5
+
+export DOCKER_IMAGE='allenday/bwa-http-docker:http'
+export BWA_FILES='gs://nanostream-dataflow-demo-data/reference-sequences/antibiotic-resistance-genes/*'
+
+# REQUESTER_PROJECT - project billed for downloading BWA_FILES
+# this line set it value to the active project ID
+export REQUESTER_PROJECT=$(gcloud config get-value project)
+
+# provisions internal load balancer, you can provision global one replacing next line with 'source provision_global.sh'
+source provision_internal.sh
+
+# Run setup
+[[ $1 != '-c' ]] && setup
+# Run cleanup if there is '-c' argument
+[[ $1 = '-c' ]] && cleanup
