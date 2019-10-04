204bac42f3b87eb1c488ea7219c67a9ed45b30a5
ruslan.smirnoff <vedmedka@gmail.com>
2019-10-04 13:42:39 +0300

    console client: remove pipeline code

--- NanostreamDataflowMain/main/src/main/java/com/google/allenday/nanostream/util/StringUtils.java
+++
@@ -1,8 +0,0 @@
-package com.google.allenday.nanostream.util;
-
-public class StringUtils {
-
-    public static String removeWhiteSpaces(String src){
-        return src.replaceAll("\\s+","");
-    }
-}
