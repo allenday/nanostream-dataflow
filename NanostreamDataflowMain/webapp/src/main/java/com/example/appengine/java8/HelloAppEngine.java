f217625ce143d06a625e6db2f1dffe5e64e3f18a
ruslan.smirnoff <vedmedka@gmail.com>
2019-10-08 14:54:02 +0300

    remove obsolete imports

--- NanostreamDataflowMain/webapp/src/main/java/com/example/appengine/java8/HelloAppEngine.java
+++ NanostreamDataflowMain/webapp/src/main/java/com/example/appengine/java8/HelloAppEngine.java
@@ -18,23 +18,14 @@ package com.example.appengine.java8;
 
 // [START example]
 
-import com.google.allenday.nanostream.NanostreamPipeline;
-import com.google.allenday.nanostream.NanostreamPipelineOptions;
-import com.google.common.collect.Lists;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import com.google.api.gax.paging.Page;
 import com.google.appengine.api.utils.SystemProperty;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-
-import java.io.IOException;
-import java.util.Properties;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
 
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
 @WebServlet(name = "HelloAppEngine", value = "/hello")
@@ -50,14 +41,6 @@ public class HelloAppEngine extends HttpServlet {
 
     private String getResponseText() {
         Properties properties = System.getProperties();
-
-        String[] args = "--runner=org.apache.beam.runners.dataflow.DataflowRunner --project=tas-nanostream-test1 --streaming=true --processingMode=species --inputDataSubscription=projects/tas-nanostream-test1/subscriptions/tas-nanostream-test1-upload-subscription --alignmentWindow=20 --statisticUpdatingDelay=30 --servicesUrl=http://10.128.0.3 --bwaEndpoint=/cgi-bin/bwa.cgi --bwaDatabase=DB.fasta --kAlignEndpoint=/cgi-bin/kalign.cgi --outputCollectionNamePrefix=new_scanning --outputDocumentNamePrefix=statistic_document".split("\\s+");
-        NanostreamPipelineOptions options = PipelineOptionsFactory.fromArgs(args)
-                .withValidation()
-                .as(NanostreamPipelineOptions.class);
-
-        new NanostreamPipeline(options).run();
-
         return "Hello App Engine - Standard using "
                 + SystemProperty.version.get() + " Java "
                 + properties.get("java.specification.version");
