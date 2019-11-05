0a6ce1d9c98ae30c23075097ec5d0284c9b54975
ruslan.smirnoff <vedmedka@gmail.com>
2019-11-05 21:12:07 +0200

    launcher: pass pipeline name to template; launcher code cleanup

--- NanostreamDataflowMain/webapp/src/main/java/com/google/allenday/nanostream/LaunchDataflow.java
+++ NanostreamDataflowMain/webapp/src/main/java/com/google/allenday/nanostream/LaunchDataflow.java
@@ -2,7 +2,7 @@ package com.google.allenday.nanostream;
 
 import com.google.appengine.api.appidentity.AppIdentityService;
 import com.google.appengine.api.appidentity.AppIdentityServiceFactory;
-import com.google.apphosting.api.ApiProxy;
+import com.google.apphosting.api.ApiProxy.Environment;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,60 +18,102 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static com.google.apphosting.api.ApiProxy.getCurrentEnvironment;
+import static java.lang.String.format;
+
 @WebServlet(name = "LaunchDataflow", value = "/launch")
 public class LaunchDataflow extends HttpServlet {
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse response) throws IOException {
-        ApiProxy.Environment env = ApiProxy.getCurrentEnvironment();
-        String project = env.getAppId();
-        String bucket = "gs://" + project + "-dataflow";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        new PipelineStarter(request, response).invoke();
+    }
 
+    private class PipelineStarter {
+        private HttpServletRequest request;
+        private HttpServletResponse response;
+        private String project;
+        private String bucket;
 
-        List<String> scopes = new ArrayList<>();
-        scopes.add("https://www.googleapis.com/auth/cloud-platform");
-        final AppIdentityService appIdentity = AppIdentityServiceFactory.getAppIdentityService();
-        final AppIdentityService.GetAccessTokenResult accessToken = appIdentity.getAccessToken(scopes);
+        public PipelineStarter(HttpServletRequest request, HttpServletResponse response) {
+            this.request = request;
+            this.response = response;
 
-        JSONObject jsonObj = null;
-        try {
-            JSONObject parameters = new JSONObject();
-            JSONObject environment = new JSONObject()
-                    .put("tempLocation", bucket + "/tmp/")
-                    .put("bypassTempDirValidation", false);
-            jsonObj = new JSONObject()
-                    .put("jobName", "template-" + UUID.randomUUID().toString())
-                    .put("parameters", parameters)
-                    .put("environment", environment);
-        } catch (JSONException e) {
-            e.printStackTrace();
+            Environment env = getCurrentEnvironment();
+            project = env.getAppId();
+            bucket = "gs://" + project + "-dataflow";
         }
 
-        URL url = new URL(String.format("https://dataflow.googleapis.com/v1b3/projects/%s/templates"
-                + ":launch?gcs_path=" + bucket + "/templates/nanostream-species", project));
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setDoOutput(true);
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Authorization", "Bearer " + accessToken.getAccessToken());
-        conn.setRequestProperty("Content-Type", "application/json");
+        public void invoke() throws IOException {
+            JSONObject jsonObj = makeParams();
 
-        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-        jsonObj.write(writer);
-        writer.close();
+            HttpURLConnection connection = sendLaunchDataflowJobFromTemplateRequest(jsonObj);
 
-        int respCode = conn.getResponseCode();
-        if (respCode == HttpURLConnection.HTTP_OK) {
-            response.setContentType("application/json");
-            String line;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                response.getWriter().println(line);
+            printOutput(connection);
+        }
+
+        private JSONObject makeParams() {
+            JSONObject jsonObj = null;
+            try {
+                JSONObject parameters = new JSONObject();
+                JSONObject environment = new JSONObject()
+                        .put("tempLocation", bucket + "/tmp/")
+                        .put("bypassTempDirValidation", false);
+                jsonObj = new JSONObject()
+                        .put("jobName", getJobName())
+                        .put("parameters", parameters)
+                        .put("environment", environment);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-            reader.close();
+            return jsonObj;
+        }
 
-        } else {
-            StringWriter w = new StringWriter();
-            IOUtils.copy(conn.getErrorStream(), w, "UTF-8");
-            response.getWriter().println(w.toString());
+        private String getJobName() {
+            String pipelineName = request.getParameter("pipeline_name");
+            if (pipelineName == null || pipelineName.isEmpty()) {
+                pipelineName = "template-" + UUID.randomUUID().toString();
+            }
+            return pipelineName; 
+        }
+
+        private HttpURLConnection sendLaunchDataflowJobFromTemplateRequest(JSONObject jsonObj) throws IOException {
+            URL url = new URL(format("https://dataflow.googleapis.com/v1b3/projects/%s/templates:launch?gcs_path=%s/templates/nanostream-species",
+                    project, bucket));
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "Bearer " + getAccessToken());
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+            jsonObj.write(writer);
+            writer.close();
+            return conn;
+        }
+
+        private void printOutput(HttpURLConnection conn) throws IOException {
+            int respCode = conn.getResponseCode();
+            if (respCode == HttpURLConnection.HTTP_OK) {
+                response.setContentType("application/json");
+                String line;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    response.getWriter().println(line);
+                }
+                reader.close();
+            } else {
+                StringWriter w = new StringWriter();
+                IOUtils.copy(conn.getErrorStream(), w, "UTF-8");
+                response.getWriter().println(w.toString());
+            }
+        }
+
+        private String getAccessToken() {
+            List<String> scopes = new ArrayList<>();
+            scopes.add("https://www.googleapis.com/auth/cloud-platform");
+            final AppIdentityService appIdentity = AppIdentityServiceFactory.getAppIdentityService();
+            return appIdentity.getAccessToken(scopes).getAccessToken();
         }
     }
 }
--- NanostreamDataflowMain/webapp/src/main/webapp/index.jsp
+++ NanostreamDataflowMain/webapp/src/main/webapp/index.jsp
@@ -2,13 +2,43 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-  <link href='//fonts.googleapis.com/css?family=Marmelad' rel='stylesheet' type='text/css'>
-  <title>Hello App Engine Standard Java 8</title>
+  <link href='styles.css' rel='stylesheet' type='text/css'>
+  <title>Nanostream</title>
 </head>
 <body>
-    <h1>Hello Dataflow Manager</h1>
+    <h1>Define the following variables to run the pipeline</h1>
     <form action='/launch' method='post'>
-        <input type="submit" value='Launch template'>
+        <div>
+            <label for="pipeline_name">Pipeline name</label>
+            <input type="text" id="pipeline_name" name="pipeline_name"  title="This is the text of the tooltip"  />
+        </div>
+        <div>
+            <label for="document_name">Document name</label>
+            <input type="text" id="document_name" name="document_name" />
+        </div>
+        <div>
+            <label>Reference database</label>
+            <div>
+                <label for="reference_database_predefined">Use predefined:</label>
+                <input type="radio" name="reference-database" id="reference_database_predefined" checked>
+                <select name="reference_database_predefined_name" autofocus="true">
+                    <option value="species">Species</option>
+                    <option value="resistance_genes">Resistance genes</option>
+                </select>
+            </div>
+            <div>
+                <label for="reference_database_custom">Custom:</label>
+                <input type="radio" name="reference-database" id="reference_database_custom">
+                <input type="text" id="reference_database_custom1" />
+            </div>
+            <div>
+                <label for="alignment_window">Alignment window</label>
+                <input type="text" id="alignment_window" name="alignment_window" title="the size of the window (in wall-clock seconds) in which FastQ records will be collected for alignment" />
+            </div>
+        </div>
+        <div>
+            <input type="submit" value='Start'/>
+        </div>
     </form>
 </body>
 </html>
