8a38fb8537adc744d4845738a9c34338887de354
pseveryn <pseverin@ukr.net>
2019-11-11 17:52:15 +0200

    small fixes after adding cannabis csv parsing

--- CannabisProcessingJava/src/main/java/com/google/allenday/nanostream/cannabis/io/CannabisCsvParser.java
+++ CannabisProcessingJava/src/main/java/com/google/allenday/nanostream/cannabis/io/CannabisCsvParser.java
@@ -5,11 +5,15 @@ import com.google.allenday.genomics.core.model.GeneExampleMetaData;
 public class CannabisCsvParser extends GeneExampleMetaData.Parser {
 
     @Override
-    public GeneExampleMetaData processParts(String[] csvLineParts, String csvLine) {
-        GeneExampleMetaData geneExampleMetaData = new GeneExampleMetaData(csvLineParts[3], csvLineParts[4], csvLineParts[6], csvLine);
-        geneExampleMetaData.setCenterName(csvLineParts[0]);
-        geneExampleMetaData.setSraStudy(csvLineParts[1]);
-        geneExampleMetaData.setBioProject(csvLineParts[2]);
-        return geneExampleMetaData;
+    public GeneExampleMetaData processParts(String[] csvLineParts, String csvLine) throws CsvParseException {
+        try {
+            GeneExampleMetaData geneExampleMetaData = new GeneExampleMetaData(csvLineParts[3], csvLineParts[4], csvLineParts[6], csvLine);
+            geneExampleMetaData.setCenterName(csvLineParts[0]);
+            geneExampleMetaData.setSraStudy(csvLineParts[1]);
+            geneExampleMetaData.setBioProject(csvLineParts[2]);
+            return geneExampleMetaData;
+        } catch (RuntimeException e) {
+            throw new CsvParseException(csvLine);
+        }
     }
 }
