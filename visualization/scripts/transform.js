f94c5fd7b5084e66adf6d7f62b087501d5e6ef16
oleksandr.bushkovskyi <bushkovsky@gmail.com>
2019-11-12 12:50:00 +0200

    Fix visualizations

--- visualization/scripts/transform.js
+++ visualization/scripts/transform.js
@@ -24,7 +24,7 @@ define(function () {
       let currentNode,
         record,
         taxonomyItem,
-        taxonomyColor,
+//        taxonomyColor,
         taxonomyLevel;
 
       const root = {
@@ -38,14 +38,15 @@ define(function () {
         if (record.name.split('|').length > 3) {
           record.name = record.name.split('|')[3];
         }
+        record.taxonomy.push(record.name);
         for (j = 0; j < record.taxonomy.length; j++) {
           taxonomyItem = record.taxonomy[j];
-          taxonomyColor = record.colors[j];
+//          taxonomyColor = record.colors[j];
           taxonomyLevel = _findChild(currentNode, taxonomyItem);
           if (!taxonomyLevel) {
             taxonomyLevel = {
               name: taxonomyItem,
-              color: taxonomyColor,
+//              color: taxonomyColor,
               size: 0,
               children: []
             };
@@ -56,7 +57,7 @@ define(function () {
         }
       }
 
-      return root.children;
+      return [root];
     };
 
     return _transform;
