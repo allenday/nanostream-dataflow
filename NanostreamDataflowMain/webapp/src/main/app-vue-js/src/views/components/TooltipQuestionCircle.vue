67245712b481681e67bcf694d15bfdf1af9153c8
ruslan.smirnoff <vedmedka@gmail.com>
2020-03-24 19:21:11 +0200

    webapp: fix tooltips click

--- NanostreamDataflowMain/webapp/src/main/app-vue-js/src/views/components/TooltipQuestionCircle.vue
+++ NanostreamDataflowMain/webapp/src/main/app-vue-js/src/views/components/TooltipQuestionCircle.vue
@@ -1,5 +1,5 @@
 <template>
-        <i class="fa fa-question-circle" v-b-tooltip.auto.hover.click.v-secondary :title="title"></i>
+        <i class="fa fa-question-circle" v-b-tooltip.auto.click.hover.blur.v-secondary :title="title" tabindex="0"></i>
 </template>
 <script>
     export default {
